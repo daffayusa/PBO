b3e2c6eea6a948a20bcdb8ad72cf844e055b0fcc
daffayusa <91883246+daffayusa@users.noreply.github.com>
2023-03-09 15:54:21 +0700

    Add files via upload

---
+++ Week1/Perkenalan.java
@@ -0,0 +1,7 @@
+public class Perkenalan {
+    public static void main(String[] args) {
+        System.out.println("Halo, nama saya adalah Daffa Yusa");
+        System.out.println("Saya adalah mahasiswa Teknik Informatika Universitas Dian Nuswantoro");
+        System.out.println("Saya sedang menulis program Java");
+    }
+}
